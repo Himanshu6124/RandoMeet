46e75d7464f648c1db54e37c1ff42ba42b998b12
apple <himanshu6124@gmail.om>
2025-12-27 15:10:57 +0530

    kmp setup

---
+++ composeApp/src/iosMain/kotlin/com/example/vibechat/koin/AppModule.ios.kt
@@ -0,0 +1,9 @@
+package com.example.vibechat.koin
+
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+actual val platformModule: Module
+    get() = module {
+        single<DeviceInfo> { IOSDeviceInfo() }
+    }
