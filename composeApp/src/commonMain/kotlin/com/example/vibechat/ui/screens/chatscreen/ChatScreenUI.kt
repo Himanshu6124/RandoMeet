34ff57c2d771cf12bbeea3f2ee53fcaee0f84f89
apple <himanshu6124@gmail.om>
2025-12-28 13:34:12 +0530

    chat screen refactor

---
+++ composeApp/src/commonMain/kotlin/com/example/vibechat/ui/screens/chatscreen/ChatScreenUI.kt
@@ -0,0 +1,279 @@
+package com.example.vibechat.ui.screens.chatscreen
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.vibechat.ui.commoncomposables.HorizontalSpacer
+import com.example.vibechat.ui.commoncomposables.TextComposable
+import com.example.vibechat.ui.screens.ChatViewModel
+import com.example.vibechat.ui.screens.chatscreen.components.Message
+import com.example.vibechat.ui.screens.chatscreen.components.MessageCard
+import com.example.vibechat.ui.screens.matchscreen.ChatCardData
+import kotlinx.coroutines.launch
+import org.koin.compose.viewmodel.koinViewModel
+
+@Composable
+fun ChatScreen(
+    modifier: Modifier = Modifier,
+    userId: String,
+    isRandomMatch : Boolean,
+    chat : ChatCardData?,
+    navigateBack : ()-> Unit,
+) {
+    val viewModel :ChatViewModel = koinViewModel()
+    var inputText by remember { mutableStateOf("") }
+    val messages = viewModel.uiState.collectAsState().value.messages
+    val isOnline = viewModel.isOnline.collectAsState()
+    val isTyping = viewModel.isTyping.collectAsState()
+    val listState = rememberLazyListState()
+    val scope = rememberCoroutineScope()
+    val message =  viewModel.message.collectAsState()
+
+    fun addMessage(newMessage: Message) {
+        viewModel.addMessage(newMessage)
+        inputText = ""
+        scope.launch {
+            val index = messages.size - 1
+            if (index != -1) {
+                listState.animateScrollToItem(messages.size - 1)
+            }
+        }
+    }
+
+    LaunchedEffect(message) {
+        message.value?.let { addMessage(it) }
+    }
+
+
+//    LaunchedEffect(Unit) {
+//        viewModel.getMessages(chat?.conversationId ?: "")
+//        viewModel.connectToSocket(
+//            friendUserId = chat?.friendUserId ?: "",
+//            conversationId = chat?.conversationId ?: "",
+//            senderId = userId.orEmpty()
+//        )
+//        viewModel.sendOnlineStatus(userId ?: "", chat?.conversationId ?: "")
+//        viewModel.getUserStatus(
+//            friendId = chat?.friendUserId.orEmpty(),
+//            conversationId = chat?.conversationId.orEmpty()
+//        )
+//    }
+
+
+    fun deleteMessage(message: Message) {
+//        messages.remove(message)
+    }
+
+    Scaffold(
+        topBar = {
+            if (chat != null) {
+                ChatScreenTopBar(
+                    chat = chat,
+                    isOnline = isOnline.value,
+                    onBackPress = navigateBack,
+                    onAddFriend = {
+                        viewModel.sendFriendRequest(
+                            userId = userId.orEmpty(),
+                            friendId = chat.friendUserId
+                        )
+                    }
+                )
+            }
+        },
+    ) { padding ->
+
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(top = padding.calculateTopPadding())
+                .background(Color.Transparent)
+        ) {
+            LazyColumn(
+                modifier = Modifier.weight(1f),
+                state = listState
+            ) {
+                items(messages) { message ->
+                    MessageCard(userId, message) {
+                        deleteMessage(it)
+                    }
+                }
+            }
+            Column(modifier = Modifier.fillMaxWidth()) {
+                if (isTyping.value) {
+                    Text(
+                        "typing...",
+                        modifier = Modifier.padding(
+                            start = 20.dp,
+                            bottom = 5.dp
+                        )
+                    )
+                }
+                SendMessageButton(
+                    userId = userId ?: "",
+                    conversationId = chat?.conversationId ?: "",
+                    inputText = inputText,
+                    onTextUpdate = {
+                        inputText = it
+                        viewModel.onUserTyping(
+                            senderId = userId ?: "",
+                            conversationId = chat?.conversationId.orEmpty(),
+                            inputText = it
+                        )
+                    },
+                    onSendMessage = {
+                        viewModel.sendMessage(message = it, isRandom = isRandomMatch)
+                        addMessage(it)
+                    }
+                )
+            }
+
+        }
+    }
+}
+
+@Composable
+fun SendMessageButton(
+    userId : String,
+    conversationId :String,
+    inputText : String,
+    onTextUpdate : (String)-> Unit,
+    onSendMessage : (Message)-> Unit,
+) {
+    Row(
+    ) {
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = inputText,
+            leadingIcon =  {
+//                Icon(
+//                    imageVector = Icons.Default.Edit,
+//                    contentDescription = null
+//                ) } ,
+//            trailingIcon = {
+//                Icon(
+//                    imageVector = Icons.AutoMirrored.Filled.Send,
+//                    contentDescription = null,
+//                    modifier = Modifier.clickable {
+//
+//                        if(inputText == "") return@clickable
+//
+//                        val newMessage = Message(
+//                            message = inputText,
+//                            status = MessageStatus.SENT,
+//                            timeStamp = String.EMPTY,
+//                            senderId = userId,
+//                            conversationId = conversationId
+//                        )
+//                        onSendMessage(newMessage)
+//                    }
+//                )
+                           },
+            placeholder = {
+                TextComposable(
+                    text = "Type your message here ...",
+                    fontWeight = FontWeight.Normal,
+                ) },
+            onValueChange = onTextUpdate
+        )
+    }
+}
+
+@Composable
+fun ChatScreenTopBar(
+    chat: ChatCardData,
+    isOnline : Boolean,
+    onBackPress: ()-> Unit,
+    onAddFriend: ()-> Unit
+) {
+
+    Column {
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(top = 30.dp),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+//            Icon(
+//                imageVector = Icons.AutoMirrored.Filled.KeyboardArrowLeft,
+//                modifier = Modifier
+//                    .size(40.dp)
+//                    .clickable { onBackPress() },
+//                contentDescription = "back"
+//            )
+//
+//            GlideImage(
+//                model = chat.photoUrl,
+//                modifier = Modifier.sizeIn(maxWidth = 24.dp , maxHeight = 24.dp),
+//                contentDescription = "profile"
+//            )
+
+            HorizontalSpacer(15.dp)
+
+            Column(
+                verticalArrangement = Arrangement.SpaceAround,
+            ) {
+                TextComposable(
+                    text = chat.friendUserName,
+                    fontSize = 18.sp
+                )
+                TextComposable(
+                    text = if(isOnline) "online" else "Offline",
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Normal
+                )
+            }
+
+            Spacer(modifier = Modifier.weight(1f))
+
+//            Icon(
+//                imageVector = Icons.Outlined.AddCircle,
+//                tint = Color.Black,
+//                modifier = Modifier
+//                    .padding(end = 20.dp)
+//                    .size(30.dp)
+//                    .clickable {onAddFriend()  },
+//                contentDescription = "Friends"
+//            )
+
+//            Icon(
+//                imageVector = Icons.Outlined.Call,
+//                tint = Color.Black,
+//                modifier = Modifier
+//                    .padding(end = 20.dp)
+//                    .size(30.dp)
+//                    .clickable {  },
+//                contentDescription = "back"
+//            )
+        }
+        HorizontalDivider(
+            modifier = Modifier.fillMaxWidth(),
+        )
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/com/example/vibechat/ui/screens/chatscreen/components/MessageCard.kt
@@ -0,0 +1,82 @@
+package com.example.vibechat.ui.screens.chatscreen.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.vibechat.ui.screens.matchscreen.MessageStatus
+
+@Composable
+fun MessageCard(
+    userId :String?,
+    message: Message ,
+    onDeleteMessage : (Message)-> Unit
+){
+    Row(
+        modifier = Modifier.fillMaxWidth().padding(bottom = if(message.senderId == userId ) 0.dp else 20.dp, start = 4.dp,end = 4.dp),
+        horizontalArrangement = if(message.senderId == userId ) Arrangement.End else Arrangement.Start
+    ) {
+        Column(
+            modifier = Modifier.clickable { onDeleteMessage(message) },
+        ){
+            Text(
+                text = message.message,
+                modifier = Modifier.background(
+                    shape = RoundedCornerShape(40),
+                    color = Color.Gray
+                ).padding(10.dp)
+            )
+
+            Text(
+                modifier = Modifier.align(Alignment.End).padding(end = 10.dp),
+                text = "12:43 pm",
+                fontSize = 10.sp
+            )
+            if(message.senderId == userId ){
+                Text(
+                    modifier = Modifier.align(Alignment.End).padding(end = 10.dp),
+                    text = when(message.status){
+                        MessageStatus.SENT -> "sent"
+                        MessageStatus.DELIVERED -> "delivered"
+                        MessageStatus.READ -> "read"
+                        null -> "unknown"
+                    },
+                    fontSize = 10.sp
+                )
+            }
+        }
+    }
+}
+
+
+data class Message(
+    val id : String? = null,
+    val senderId: String?= null,
+    val message: String,
+    var status: MessageStatus? = null,
+    val conversationId: String,
+    val timeStamp: String,
+)
+
+data class OnlineStatus(
+    val senderId: String?= null,
+    val conversationId: String,
+    val online: Boolean = false,
+)
+
+data class TypingStatus(
+    val senderId: String?= null,
+    val conversationId: String,
+    val typing: Boolean = false ,
+)
